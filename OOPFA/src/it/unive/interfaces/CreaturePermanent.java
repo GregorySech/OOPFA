2ff23e456ed84db5041af9d58d40efb601af2889
giacomo <giacomo@giacomo-pc>
2017-03-13 15:11:36 +0100

    Aggiunte un po' di robe sui permanent, fatto interfaccia permanent e creaturePermanent (2)

--- OOPFA/src/it/unive/interfaces/CreaturePermanent.java
+++ OOPFA/src/it/unive/interfaces/CreaturePermanent.java
@@ -9,11 +9,57 @@ package it.unive.interfaces;
  *
  * @author giacomo
  */
-public interface CreaturePermanent {
+public interface CreaturePermanent extends Permanent{
+      /**
+       * ritorna i punti vita della creatura
+       * @return 
+       */
       public int getHp();
+      /**
+       * ritorna i punti di attacco della creatura
+       * @return 
+       */
       public int getAttack();
+      /**
+       * ritorna true se la creatura è tappata
+       * @return 
+       */
       public boolean isTapped();
+      /**
+       * ritorna true se la creatura è in difesa
+       * @return 
+       */
+      public boolean isDefender();
+      /**
+       * setta la creatura come difensore o no 
+       * @param def 
+       */
+      public void setDefender(boolean def);
+      /**
+       * tappa la creatura
+       */
+      public void tap();
+      /**
+       * untappa la creatura
+       */
+      public void untap();
+      /**
+       * difendi il Player da attacker
+       * @param attacker 
+       */
       public void defenseFrom(CreaturePermanent attacker);
-      public Player getChief();
-      
+      /**
+       * attacca il Player enemy
+       * @param enemy 
+       */
+      public void attack(Player enemy);
+      /**
+       * ripristina i punti vita
+       */
+      public void restoreHp();
+      /**
+       * ritorna la Stringa contenente le info della creatura
+       * @return 
+       */
+      public String toString();
 }
